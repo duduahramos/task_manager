ef53f8240c0d0cd13d2db7760c45b6b2449f3bf5
Eduardo Ramos <eduardoahramos@outlook.com>
2025-02-02 14:28:23 -0300

    feat: first commit - gitignore e iniciando models e db context

---
+++ TaskManagerApi/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerApi.Models;
+
+namespace TaskManagerApi.Context
+{
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<UserTask> UserTasks { get; set; }
+    }
+}
